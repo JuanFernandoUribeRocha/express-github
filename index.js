ac5c3557dad9e38d87934d292aaab1f43ad0d76c
JuanFernandoUribeRocha <fernandouribe.tech@gmail.com>
2026-04-15 18:55:15 -0700

    Se creó el SELECT para traer la información de los alumnos con /alumnos

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const pool = require('./db');
 const app = express();
 
+//app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
@@ -13,6 +15,27 @@ pool.connect()
   .catch((err) => {
     console.error('Error de conexión', err);
   });
+
+app.get('/usuario', (req, res)=> {
+  const usuario = {
+    id: 1,
+    nombre: 'Juan',
+    rol: 'Administrador'
+  };
+
+  res.json(usuario);
+});
+
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
